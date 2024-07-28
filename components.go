dbd14e4b84103fb06c573cc920f7caec0adec465
Harris Bisset <hb233@st-andrews.ac.uk>
2024-07-28 16:42:21 +0100

    nil check fix

--- components.go
+++ components.go
@@ -41,31 +41,28 @@ func (c Component) Render(w http.ResponseWriter, req *http.Request) {
 	//check if view assigned
 	if c.View == nil {
 		log.Printf("%s view not assigned", c.Name)
-		return
+	} else {
+		// try to render page
+		err := c.View.Render(req.Context(), w)
+		if err == nil {
+			log.Printf("Rendered %s", c.Name)
+			return
+		}
+		log.Printf("%s occured on %s: %s", RenderFail.String(), c.Name, err)
 	}
-
-	// try to render page
-	err := c.View.Render(req.Context(), w)
-	if err == nil {
-		log.Printf("Rendered %s", c.Name)
-		return
-	}
-	log.Printf("%s occured on %s: %s", RenderFail.String(), c.Name, err)
 
 	//check if error assigned
 	if c.Err == nil {
 		log.Printf("%s error not assigned", c.Name)
-		return
+	} else {
+		// try to render 404
+		err := c.Err.Render(req.Context(), w)
+		if err == nil {
+			log.Printf("404 Rendered %s", c.Name)
+			return
+		}
+		log.Printf("%s occured on %s: %s", ErrorFail.String(), c.Name, err)
 	}
-
-	// try to render 404
-	err = c.Err.Render(req.Context(), w)
-	if err == nil {
-		log.Printf("404 Rendered %s", c.Name)
-		return
-	}
-
-	log.Printf("%s occured on %s: %s", ErrorFail.String(), c.Name, err)
 }
 
 func HandleComponent(p Path, url string, com func(w http.ResponseWriter, r *http.Request)) {
