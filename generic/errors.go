8e91a08b1fd1ab07e5ed382f33b9288d94aa0911
harrisbisset <hb233@st-andrews.ac.uk>
2024-10-22 00:07:42 +0100

    generic fiber

--- generic/errors.go
+++ generic/errors.go
@@ -9,10 +9,20 @@ import (
 	"github.com/harrisbisset/fnet"
 )
 
-type BuildError struct{}
+type (
+	DefaultError struct{}
+	BuildError   struct{}
+)
+
+var (
+	GenericBuildError    = fnet.NewError("build error", BuildError{}).Code(http.StatusInternalServerError).Build()
+	DefaultResponseError = fnet.NewError("default error", BuildError{}).Code(http.StatusInternalServerError).Build()
+)
+
+func (d DefaultError) Render(ctx context.Context, w io.Writer) error {
+	return errors.New("<div style='margin:auto;'>404 - Page not Found</div>")
+}
 
 func (b BuildError) Render(ctx context.Context, w io.Writer) error {
 	return errors.New("<div style='margin:auto;'>404 - Page not Found</div>")
 }
-
-var GenericBuildError = fnet.NewError("build error", BuildError{}).Code(http.StatusInternalServerError).Build()
