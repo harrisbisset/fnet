98e114723694714863e3d9cd2dc651df46af8f9a
harrisbisset <hb233@st-andrews.ac.uk>
2024-12-29 15:26:50 +0000

    v2

--- components.go
+++ components.go
@@ -8,52 +8,66 @@ import (
 
 type (
 	Component struct {
-		ContentType string
-		Response    func(w http.ResponseWriter, r *http.Request)
-
-		Handler *fnettempl.ComponentHandler
+		Response Handler
+		Handler  *fnettempl.ComponentHandler
 	}
 
-	ComponentOption func(*Component)
+	DataComponent[T any] struct {
+		Response func(v T) Handler
+		Data     T
+	}
+
+	ComponentOption            func(*Component)
+	DataComponentOption[T any] func(*DataComponent[T])
 )
 
 func (c Component) Render(w http.ResponseWriter, r *http.Request) {
-	if c.Handler == nil {
+	if c.Handler != nil {
 		c.Handler.ServeHTTP(w, r)
 		return
 	}
 
-	if c.
+	if c.Response != nil {
+		c.Response(w, r)
+		return
+	}
+}
 
-	c.Response(w, r)
+func (c *DataComponent[T]) RenderWithData(v T) func(w http.ResponseWriter, r *http.Request) {
+	c.Data = v
+	return c.Response(c.Data)
 }
 
 func NewComponent(opts ...ComponentOption) Component {
-	c := &Component{
-		ContentType: "text/html; charset=utf-8",
-	}
-
+	c := &Component{}
 	for _, o := range opts {
 		o(c)
 	}
-
 	return *c
 }
 
-func WithContentType(t string) ComponentOption {
-	return func(c *Component) {
-		c.ContentType = t
+func NewDataComponent[T any](opts ...DataComponentOption[T]) *DataComponent[T] {
+	dc := &DataComponent[T]{}
+	for _, o := range opts {
+		o(dc)
 	}
+	return dc
 }
 
-func WithTempl(v fnettempl.Component, opts ...func(*fnettempl.ComponentHandler)) ComponentOption {
+func WithTempl(v fnettempl.Component) ComponentOption {
 	return func(c *Component) {
 		c.Handler = fnettempl.Handler(v)
 	}
 }
 
-func WithRender(fn func(w http.ResponseWriter, r *http.Request)) ComponentOption {
+func WithRender(fn Handler) ComponentOption {
 	return func(c *Component) {
+		c.Response = fn
+	}
+}
 
+func SetRender[T any](fn func(v T) Handler) DataComponentOption[T] {
+	return func(dc *DataComponent[T]) {
+		dc.Response = fn
 	}
 }
--- example/main.go
+++ example/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/harrisbisset/fnet/example/components"
+	"github.com/harrisbisset/fnet/example/models"
 )
 
 func main() {
-	http.HandleFunc("/", components.IndexPage.Render)
-	http.HandleFunc("/db", components.DatabasePage.Render)
+	db := models.DatabaseRecord{
+		Name: "Database Record Name",
+	}
 
+	http.HandleFunc("/", components.IndexPage.Render)
+	http.HandleFunc("/db", components.DatabasePage.RenderWithData(db))
+
+	log.Print("listening")
 	http.ListenAndServe(":8080", nil)
 }
